52251be7010d8d6017d0c9e23db8e0f0a2f94a4f
sara limbu <limbusaraswati@gmail.com>
2019-05-06 14:02:03 +1000

    Prep for deploy

--- db_config.rb
+++ db_config.rb
@@ -6,4 +6,5 @@ options = {
   database: 'cheerups'
 }
 
-ActiveRecord::Base.establish_connection(options)
+# ActiveRecord::Base.establish_connection(options)
+ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || options)
